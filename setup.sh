16c420e389610f8d32f036d91fd720fe0065b42f
Chris Komlenic <komlenic@gmail.com>
2019-03-11 14:19:03 -0400

    Remove git directory on install

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ echo "===> Setting up project..."
 mkdir -p dist/css dist/fonts dist/img dist/js src/scss src/fonts src/img src/js
 cp ./node_modules/bootstrap-sass/assets/stylesheets/bootstrap/_variables.scss ./src/scss/_variables-custom.scss
 cp ./node_modules/bootstrap-sass/assets/stylesheets/_bootstrap.scss ./src/scss/_bootstrap-custom.scss
+rm -rf ./.git
 
 echo "===> Running gulp for the first time..."
 gulp
